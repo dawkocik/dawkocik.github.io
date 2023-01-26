36e8d85cb7505fae16d1e20d1f8032ae8ee3ffd5
Dawid Kocik <dawkocik@gmail.com>
2023-01-26 17:06:38 +0100

    😎 initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,51 @@
+<template>
+  <button><slot /></button>
+</template>
+
+<style>
+button {
+  cursor: pointer;
+  width: max-content;
+  padding-left: 23px;
+  padding-right: 23px;
+  height: 76px;
+  font-family: "Joystix";
+  font-style: normal;
+  font-size: 42px;
+  line-height: 50px;
+  text-align: center;
+  letter-spacing: -0.015em;
+  color: #000;
+  background: #fff;
+  border: 3px solid #000;
+  border-radius: 4px;
+  position: relative;
+  top: 0;
+  left: 0;
+  transition: all 0.3s;
+}
+
+button::after {
+  content: "";
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  background-color: #768a7e;
+  border: 3px solid #000;
+  border-radius: 4px;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  transition: all 0.3s;
+}
+
+button:hover {
+  top: 2.5px;
+  left: 2.5px;
+}
+
+button:hover::after {
+  top: -1px;
+  left: -1px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Button from "./Button.vue"
+</script>
+
+<template>
+  <div class="container">
+    <Button>Home</Button>
+    <Button>Projects</Button>
+    <Button>Contact</Button>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  border-top: 2px solid;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.17);
+  position: fixed;
+  bottom: 0;
+}
+
+Button {
+  margin-right: 40px;
+  margin-top: 6px;
+  margin-bottom: calc(6px + 5px + 3px);
+}
+</style>
